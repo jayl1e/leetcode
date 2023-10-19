9f77c1ff01cfac6fd9948d1a5e8e8c17371522c0
lijie0123 <lijie0123lj@gmail.com>
2023-10-19 20:48:26 +0800

    add 141
    Signed-off-by: lijie0123 <lijie0123lj@gmail.com>

---
+++ 141. Linked List Cycle/s.go
@@ -0,0 +1,31 @@
+package main
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle1(head *ListNode) bool {
+	fast, slow := head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			return true
+		}
+	}
+	return false
+}
+
+func hasCycle(head *ListNode) bool {
+	visited := make(map[*ListNode]struct{})
+	for head != nil {
+		if _, ok := visited[head]; ok {
+			return true
+		}
+		visited[head] = struct{}{}
+		head = head.Next
+	}
+	return false
+}
